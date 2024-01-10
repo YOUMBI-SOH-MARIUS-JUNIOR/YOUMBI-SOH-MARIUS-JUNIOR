16e9e1ca37e63d9517c41c277037cf1f2d18c162
Youmbi Soh Marius Junior <156077526+YOUMBI-SOH-MARIUS-JUNIOR@users.noreply.github.com>
2024-01-10 15:54:29 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+//import 'package:chatgptexample/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+// Entry Point
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key ? key
+  }): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Disable debug banner
+      debugShowCheckedModeBanner: false,
+      // set title name
+      title: 'ChatGPT App',
+      // select theme
+      theme: ThemeData(primarySwatch: Colors.green, ),
+      // Here we are calling a Screen named ChatScreen
+      home: ChatScreen(), );
+  }
+}
+
+
+
